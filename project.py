ed8062285e774d12c5aab5215c5d092b7c2a8ee6
sapryk1n <32887562+sapryk1n@users.noreply.github.com>
2018-09-03 17:13:40 +1000

    Update project.py

--- project.py
+++ project.py
@@ -4,7 +4,7 @@ import time
 
 while True:
     vk = vk_api.VkApi(token = "a368e2d6b1814280fcd81d06b5f1208730c6dbdfb2c51f96bf088f393a470cc683fa57445c01217d42df7")
-    delta = datetime.timedelta(hours=11, minutes=0)  # разница от UTC.
+    delta = datetime.timedelta(hours=10, minutes=0)  # разница от UTC.
     t = (datetime.datetime.now(datetime.timezone.utc) + delta)  # Присваиваем дату и время переменной «t»
     nowtime = t.strftime("%H:%M")  # текущее время
     nowdate = t.strftime("%d.%m.%Y")  # текущая дата
